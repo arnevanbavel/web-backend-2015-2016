3c3bc850712b0256898ca0a087faccc78b504ef7
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2016-04-01 15:12:06 +0200

    eerste opdracht

---
+++ opdracht-comments.php
@@ -0,0 +1,20 @@
+<?php
+    $naam = 'Arne Van Bavel'
+    
+    /*
+    Arne Van Bavel
+    arne.vanbavel@student.kdg.be
+    */
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>opdracht-comments</title>
+	</head>
+	<body>
+		<h1>opdracht-comments</h1>
+		<p><?php echo 'Arne Van Bavel' ?></p>
+        <p><?= $naam ?></p>
+	</body>
+</html>
