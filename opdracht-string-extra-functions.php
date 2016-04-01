9b130821508d38cc477cfa0e4c4cd918db7b057f
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2016-04-01 18:06:07 +0200

    opdracht if afgewerkt

--- opdracht-string-extra-functions.php
+++ opdracht-string-extra-functions.php
@@ -1,5 +1,17 @@
 <?php
+    $fruit = "kokosnoot";
+    $letter = "o";
+    $pos = strpos($fruit,$letter);
 
+    $fruit1 = "ananas";
+    $letter1 = "a";
+    $pos1 = strrpos($fruit,$letter);
+    $FRUIT1 = strtoupper($fruit1);
+
+    $lettertje = "e";
+    $cijfertje = "3";
+    $langstewoord = "zandzeepsodemineralenwatersteenstralen";
+    $nieuwezin = str_replace($lettertje,$cijfertje,$langstewoord);
 
 ?>
 
@@ -10,6 +22,9 @@
 	</head>
 	<body>
 		<h1>title</h1>
-		<?php echo 'Arne Van Bavel' ?>
+		<p> positie o in kokosnoot: <?php echo $pos ?></p>
+        <p> positie laatste a in ananas: <?php echo $pos1 ?></p>
+        <p> ananas in grote letters: <?php echo $FRUIT1 ?></p>
+        <p> nieuwe zin: <?php echo $nieuwezin ?></p>
 	</body>
 </html>
