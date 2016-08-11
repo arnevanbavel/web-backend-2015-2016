e8117a4b99cbc9518f759a7957b01691b260b534
Arne Van Bavel <arne.vanbavel@student.kdg.be>
2016-08-11 16:16:22 +0200

    hackernes database aangemaakt

--- laravelhackersnews/quickstart/database/migrations/2016_08_11_134800_create_votes_table.php
+++ laravelhackersnews/quickstart/database/migrations/2016_08_11_134800_create_votes_table.php
@@ -17,7 +17,6 @@ class CreateVotesTable extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('artikel_id')->unsigned();
             $table->timestamps();
-
             $table->foreign('user_id')
                     ->references('id')
                     ->on('users');
